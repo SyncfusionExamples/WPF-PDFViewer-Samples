5a213fcb674f6854cf415ba11210cd85452872d4
Yathavakrishnan Mohan <yathavakrishnan.mohan@syncfusion.com>
2024-10-15 14:09:41 +0530

    903535 - Feed back changes

--- How-to-export-the-pages-in-pdf-document-to-multi-page-tiff-image/MultipageTiff/Properties/AssemblyInfo.cs
+++ How-to-export-the-pages-in-pdf-document-to-multi-page-tiff-image/MultipageTiff/Properties/AssemblyInfo.cs
@@ -7,11 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-//[assembly: AssemblyTitle("GettingStarted_2008")]
 [assembly: AssemblyDescription("")]
-//[assembly: AssemblyConfiguration("")]
-//[assembly: AssemblyCompany("Microsoft")]
-//[assembly: AssemblyProduct("GettingStarted_2008")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -30,17 +26,6 @@ using System.Windows;
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-
-//[assembly: ThemeInfo(
-//    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-//    //(used if a resource is not found in the page, 
-//    // or application resource dictionaries)
-//    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-//    //(used if a resource is not found in the page, 
-//    // app, or any theme specific resource dictionaries)
-//)]
-
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -51,5 +36,3 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
